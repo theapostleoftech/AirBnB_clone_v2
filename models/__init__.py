c7cad89016d6e33db23d6555958fdb1b2fc1f737
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-17 16:55:36 +0100

    DBStorage - States ad Cities

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ import os
 from models.engine.file_storage import FileStorage
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.db_storage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
