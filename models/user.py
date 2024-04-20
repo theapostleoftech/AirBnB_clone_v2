92cd4a2e01fadc87ba3c8ec42ece8e73f4587e08
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-20 23:53:02 +0100

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -3,11 +3,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    if (storage_engine == 'db'):
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
