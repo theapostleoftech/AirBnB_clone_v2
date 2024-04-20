64fc7d97389aa7d027853612f3bdc5b29a68912a
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-20 23:02:36 +0100

    Delete objects

--- models/engine/f3ile_storage.py
+++ models/engine/f3ile_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns list of objects of one type of class"""
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            return {
+                key: val for key,
+                val in FileStorage.__objects.items()
+                if isinstance(val, cls)
+                }
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects. Does nothing if obj is None."""
+        if obj is not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
