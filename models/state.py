6491ff6bb6e70c94401533ee0d798ad79ea090a8
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-04-21 00:46:47 +0100

    DBStorage - User

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -22,6 +23,11 @@ class State(BaseModel, Base):
 
     @property
     def cities(self):
-        from models import storage
-        cities = storage.all(City).values()
-        return [city for city in cities if city.state_id == self.id]
+        result = []
+        for j, i in models.storage.all(models.city.City).items():
+            if (i.state_id == self.id):
+                result.append(i)
+        return result
+        # from models import storage
+        # cities = storage.all(City).values()
+        # return [city for city in cities if city.state_id == self.id]
