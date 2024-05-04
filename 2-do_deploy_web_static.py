db9304224ce95f7059484e4a6f1f2ff7014f461d
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-04 01:04:49 +0000

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,62 @@
+
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers
+"""
+from fabric.api import env, run, put, local
+from os.path import exists
+
+# Define your server IPs and user
+env.hosts = ['52.86.208.90', '54.166.135.118']
+env.user = 'ubuntu'
+env.key_filename = 'authorized_keys'  # Define your SSH private key here
+
+
+def do_deploy(archive_path):
+    """
+    Deploys the archive to the web servers.
+    """
+    if not exists(archive_path):
+        return False
+
+    # Extract file name and directory paths from archive_path
+    file_name = archive_path.split("/")[-1]
+    no_ext = file_name.split(".")[0]
+    dest_path = "/data/web_static/releases/" + no_ext
+
+    try:
+        # Upload archive to the /tmp/ directory
+        put(archive_path, "/tmp/" + file_name)
+
+        # Create directory where we will uncompress
+        run("mkdir -p {}".format(dest_path))
+
+        # Uncompress the archive to the folder on the server
+        run("tar -xzf /tmp/{} -C {}".format(file_name, dest_path))
+
+        # Delete the archive from the server
+        run("rm /tmp/{}".format(file_name))
+
+        # Move contents out of the 'web_static' folder from the tar archive
+        run("mv {0}/web_static/* {0}/".format(dest_path))
+
+        # Remove the empty directory
+        run("rm -rf {}/web_static".format(dest_path))
+
+        # Delete the old symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(dest_path))
+
+        print("New version deployed!")
+        return True
+    except:
+        return False
+
+# # Optionally, add a helper function to run it all
+# def deploy():
+#     archive_path = do_pack()
+#     if archive_path is None:
+#         return False
+#     return do_deploy(archive_path)
