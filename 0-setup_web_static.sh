7ee12881161fa1e6ff9088ce39d1c850419d09f7
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-04 00:26:40 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -35,36 +35,3 @@ sudo service nginx restart
 
 # Exit successfully
 exit 0
-
-# # Check and install nginx if not installed
-# if ! dpkg -s nginx >/dev/null 2>&1; then
-#     sudo apt-get update
-#     sudo apt-get install -y nginx
-# fi
-
-# # Create directories if not available
-# sudo mkdir -p /data/web_static/releases/test/
-# sudo mkdir -p /data/web_static/shared/
-
-# # Creating a test HTML
-# cat <<E0F | sudo tee /data/web_static/releases/test/index.html > /dev/null
-# <html>
-# <head></head>
-# <body>Holberton School</body>
-# </html>
-# E0F
-
-# # Create symbolic link
-# sudo rm -rf /data/web_static/current
-# sudo lm -sf /data/web_static/releases/test/ /data/web_static/current
-
-
-# # Grant ownership
-# sudo chown -R ubuntu:ubuntu /data/
-
-# nginx_config="/etc/nginx/sites-available/default"
-# sudo sed -i '38i \\\tlocation /hbnb_static/ {\n\t\talias /data_web_static/current/;\n\t}\n' "$nginx_config"
-
-# sudo service nginx restart
-
-# exit 0
