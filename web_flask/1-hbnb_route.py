538c2ad95d7c2375a40e0e7dc8f38cb61d330ab0
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-18 13:37:22 +0100

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Start a web application to listen on port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Hello route listening on 0.0.0.0 port 5000
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    hbnb route listening on 0.0.0.0 port 5000
+    """
+    return 'HBNB'
+
+
+if '__name' == '__main__':
+    app.run(host='0.0.0.0', port=5000)
