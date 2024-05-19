992b5049e852e67c462fb8f8796ec459f9e701c2
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-19 15:20:45 +0100

    Is it a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+listening on 0.0.0.0:5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    This function displays "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb_route():
+    """
+    This functions displays HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c_route(text):
+    """
+    This functions displays C followed by
+    the value of the text variable
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python_route(text):
+    """
+    This function displays Python followed by
+    the value of the text variable
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def hello_number_route(n):
+    """
+    This function displays n is a number
+    if n is an integer
+    """
+    if int(n):
+        return "{} is a number".format(n)
+    else:
+        return '4ot Found', 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
