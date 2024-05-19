215a8aefecc73973dec4bec06c45c1fef9dc7f65
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-19 15:03:06 +0100

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+listening on 0.0.0.0:5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    This function displays "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb_route():
+    """
+    This functions displays HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c_route(text):
+    """
+    This functions displays C followed by
+    the value of the text variable
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('python/<text>', strict_slashes=False)
+def hello_python_route(text):
+    """
+    This function displays Python followed by
+    the value of the text variable
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
