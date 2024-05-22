2f604bd007bc8c1941ae45e5fa4fa767ef2050a1
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-22 19:26:30 +0100

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script that lists all the states after
+starting the web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def terminate_db(exception):
+    """
+    This functin closes the database connection
+    after each request
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states_route():
+    """
+    This function lists states in a HTML page
+    sorted by name
+    """
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
