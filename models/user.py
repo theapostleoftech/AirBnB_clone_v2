148acc19c35b38eda5022336832e6e1a71998ba1
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-22 18:54:19 +0100

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -9,11 +9,6 @@ from models.place import Place
 
 class User(BaseModel, Base):
     """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
     """
     __tablename__ = "users"
 
