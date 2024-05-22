4ec4c5ca9784786f4feac74b99c43acecf0c77dd
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-22 19:06:46 +0100

    DBStorage - Amenity and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -8,8 +8,6 @@ from models.place import Place, place_amenity
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
-    Attributes:
-        name: input name
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
