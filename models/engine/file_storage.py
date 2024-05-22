6cd97928b74a6ab767bbf19dd0fee568ea09fa27
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-22 19:07:56 +0100

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,9 +13,6 @@ from models.review import Review
 class FileStorage:
     """This class serializes instances to a JSON file and
     deserializes JSON file to instances
-    Attributes:
-        __file_path: path to the JSON file
-        __objects: objects will be stored
     """
     __file_path = "file.json"
     __objects = {}
