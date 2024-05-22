006dc824311fe3a8cea1a7b9446787d2db7b57b7
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-22 19:01:33 +0100

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -6,10 +6,6 @@ from sqlalchemy import Column, String, ForeignKey
 
 class Review(BaseModel, Base):
     """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
     """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
